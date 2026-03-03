3e4c6ec48280ecbe2f6ae567975bb705a8b0620b
Devendra Singh <devsingh.sap@gmail.com>
2026-03-03 15:55:43 +0000

    Running state

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,4 @@
-const cds = require('@sap/cds');
-const proxy = require('@sap/cds-odata-v2-adapter-proxy');
-
-cds.on('bootstrap', app => {
-    app.use(proxy());
-});
-
+const cds = require("@sap/cds");
+const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
+cds.on("bootstrap", (app) => app.use(cov2ap()));
 module.exports = cds.server;
